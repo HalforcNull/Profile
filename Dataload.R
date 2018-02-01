faccb7633c89458c1c50ca55a6599b2f275ae055
HalforcNull <yaorunan@gmail.com>
2018-02-01 12:30:00 -0600

    Adding new data load script

---
+++ Dataload.R
@@ -0,0 +1,29 @@
+library(RMySQL)
+
+listOfVars <- list()
+if(!(   'DB_USERNAME' %in% listOfVars &&
+        'DB_PASSWORD' %in% listOfVars &&
+        'DB_HOSTNAME' %in% listOfVars &&
+        'DB_SCHEMA' %in% listOfVars) )
+{
+    cat('Data base login info is missing')
+}
+
+con <- dbConnect(MySQL(),
+    user = DB_USERNAME,
+    password = DB_PASSWORD,
+    host = DB_HOSTNAME,
+    dbname= DB_SCHEMA)
+dbDisconnect(con)     
+     
+options(sqldf.driver = "SQLite") 
+
+
+
+
+
+device_weight <- dbGetQuery(conn = con, statement = 
+                "SELECT user_id, DATE(date_recorded) AS date_recorded, weight, substring(date_recorded, 1, 7) AS yearmo
+                FROM device_weight
+                WHERE weight > 100 AND weight < 1000 AND user_id !=0;
+                ")   
